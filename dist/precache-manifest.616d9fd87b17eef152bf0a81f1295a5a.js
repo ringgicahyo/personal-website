00e76db74ff9fcd11c12541e3b26dbd97a846fd8
Ringgi Cahyo Dwiputra <ringgicahyo@gmail.com>
2020-03-25 03:15:26 +0700

    Update design for personal website

---
+++ dist/precache-manifest.616d9fd87b17eef152bf0a81f1295a5a.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "9da0111c6a8483db882d",
+    "url": "/personal-website/css/app.58b5f59d.css"
+  },
+  {
+    "revision": "9da0111c6a8483db882d",
+    "url": "/personal-website/js/app.4b8836e0.js"
+  },
+  {
+    "revision": "6641e64aefc640bcf32c",
+    "url": "/personal-website/js/chunk-52e4de86.44311a54.js"
+  },
+  {
+    "revision": "4a1015279998b224ca74",
+    "url": "/personal-website/css/chunk-ebdaf33a.79c177b7.css"
+  },
+  {
+    "revision": "4a1015279998b224ca74",
+    "url": "/personal-website/js/chunk-ebdaf33a.8bcd2a35.js"
+  },
+  {
+    "revision": "8a64226f2348ffba3aa6",
+    "url": "/personal-website/js/chunk-vendors.c7a91b52.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/personal-website/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/personal-website/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/personal-website/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/personal-website/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/personal-website/img/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/personal-website/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/personal-website/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/personal-website/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/personal-website/img/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/personal-website/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "93b3adae1f593066585c85a7474faa2e",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/personal-website/img/brand/blue.png"
+  },
+  {
+    "revision": "fa4aa9d9936d553b6d792f735dc0295f",
+    "url": "/personal-website/favicon.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/personal-website/img/brand/favicon.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/personal-website/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "fa4aa9d9936d553b6d792f735dc0295f",
+    "url": "/personal-website/img/brand/r_logo_red.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/personal-website/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "965a4c91d408aa0435094e65b88308d6",
+    "url": "/personal-website/img/brand/r_sign.png"
+  },
+  {
+    "revision": "d778e4285241e270219ec32a09148053",
+    "url": "/personal-website/img/brand/r_logo_simple.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/personal-website/img/brand/white.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/personal-website/img/favicon.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/personal-website/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/personal-website/img/theme/landing.jpg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/personal-website/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/personal-website/img/theme/profile.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/personal-website/img/theme/promo-1.png"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/personal-website/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/personal-website/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/personal-website/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/personal-website/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/personal-website/img/theme/team-2-800x800.jpg"
+  }
+];
